18061771bfd690daade0e64fdeb5aa5f6bd59b81
RodrigoBorges5 <rodrigo.borges@best-eu.org>
2025-12-18 17:18:32 +0000

    dcker

--- newservice/newservice/settings.py
+++ newservice/newservice/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,15 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5pvpw(i!2*or74jjcgg@+)e@t7upnrp#+3(l-9@b@k22y3hzw='
+SECRET_KEY = os.getenv("DJAGO")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned in production!
+DEBUG = os.getenv("DEBUG") == "True"    
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
-# Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -73,9 +75,14 @@ WSGI_APPLICATION = 'newservice.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
@@ -97,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
--- newservice/service/views.py
+++ newservice/service/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("You're at the service index.")
+def idex(request):
+    return HttpResponse("You're at the service indexs.")
