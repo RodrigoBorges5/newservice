be73603034ed9352f63aac6addbf757c4f725e47
RodrigoBorges5 <rodrigo.borges@best-eu.org>
2026-02-28 12:07:13 +0000

    r

--- newservice/newservice/settings.py
+++ newservice/newservice/settings.py
@@ -148,8 +148,13 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
     ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [],  # Sem autenticação por agora (mock auth)
-    "DEFAULT_PERMISSION_CLASSES": [],  # Sem permissões por agora
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "service.middleware.IsAll",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
 }
